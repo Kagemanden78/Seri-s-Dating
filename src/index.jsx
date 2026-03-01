ff88e929ccfe97a3192a18464abee63fee7659ce
Kagemanden78 <kagemanden78@gmail.com>
2026-03-01 17:18:33 +0100

    Create React entry point in src/index.jsx

---
+++ src/index.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App = () => <h1>Welcome to Seri's Dating App!</h1>;
+
+ReactDOM.render(<App />, document.getElementById('root'));
